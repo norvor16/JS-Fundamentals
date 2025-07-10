4f34a3c7889dd594277ffb1fae1fa1970c08a98c
MIRIAM NORVOR <vilmiriam@gmail.com>
2025-07-10 14:03:58 +0000

    Updated script in 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const arg = process.argv[2];
 const times = parseInt(arg);
 
-if (isNaN(times) || times <= 0) {
+if (isNaN(times) || times === 0) {
     console.log("Missing number of occurrences");
-} else {
+} else if (times > 0) {
     let count = 0;
     while (count < times) {
         console.log("C is fun");
